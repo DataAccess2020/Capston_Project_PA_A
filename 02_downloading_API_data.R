d8c3ca4c16a1b279f6c37c7399dc7605dd1a4622
PaoloAmantini <95076443+PaoloAmantini@users.noreply.github.com>
2022-05-13 11:35:33 +0200

    offset fixed,  API data downloaded and stored.

--- 02_downloading_API_data.R
+++ 02_downloading_API_data.R
@@ -4,8 +4,8 @@ query<- import("links3.csv")
 
 s_query <- as.data.frame  (query$key[1:3])
 s_query$link <- c("https://api.propublica.org/congress/v1/bills/search.json?query=climate%20change",
-                  "https://api.propublica.org/congress/v1/bills/search.json?query=carbon%20emissions",
-                  "https://api.propublica.org/congress/v1/bills/search.json?query= sea%20level%20rise")
+                  "https://api.propublica.org/congress/v1/bills/search.json?query=carbon%20emission",
+                  "https://api.propublica.org/congress/v1/bills/search.json?query=sea%20level%20rise")
 
 colnames(s_query) <- c("key", "que_link")
 
@@ -15,24 +15,45 @@ q_offset <- as.character(v_off)
 
 result <- vector(mode = "list")
 
-i= 0
+i <-  0
 for (i in 1:length(q_offset)){ 
-  step <- RCurl::getURL(paste0(s_query$que_link, ",","offset=", q_offset[i]),
-                        httpheader = c(s_query$key))
-  result <- append.xmlNode(result, step)
+  step <- RCurl::getURL(
+                        paste0(s_query$que_link[1], "&","offset=", q_offset[i]),
+                               httpheader = c(s_query$key[1]))
+  result <- append(result, step)
   Sys.sleep(1)
   cat(q_offset[i]," ")
 }
 
+result1 <- vector(mode = "list")
+i <-  0
+for (i in 1:length(q_offset)){ 
+  step <- RCurl::getURL(
+                        paste0(s_query$que_link[2], "&","offset=", q_offset[i]),
+                         httpheader = c(s_query$key[2]))
+  result1 <- append(result1, step)
+  Sys.sleep(1)
+  cat(q_offset[i]," ")
+}
 
-del <- seq(0,150, by = 3)
-prov<- result[-c(del)]
+result2 <- vector(mode = "list")
+i <-  0
+for (i in 1:length(q_offset)){ 
+  step <- RCurl::getURL(
+                         paste0(s_query$que_link[3], "&","offset=", q_offset[i]),
+                         httpheader = c(s_query$key[3]))
+  result2 <- append(result2, step)
+  Sys.sleep(1)
+  cat(q_offset[i]," ")
+}
 
-bills_list <- list()
+d_api<- c(result,result1,result2)
+
+r_list <- list()
 
 i <- 1
-for (i in 1:100){
-  step <- fromJSON(prov[[i]])
-  bills_list[[length(bills_list)+1]] <- step
+for (i in 1:length(d_api)){
+  step <- fromJSON(d_api[[i]])
+  r_list[[length(r_list)+1]] <- step
 }
 
--- 03_extracting_data.R
+++ 03_extracting_data.R
@@ -1,94 +1,117 @@
 # extracting from the registers
 
-sponsor_list <- list()
+sub_list <- list()
 i <- 1
-for (i in 1:100){
-  step <- bills_list[[i]][[3]][[1]][[3]]
-  sponsor_list[[length(sponsor_list)+1]] <- step
+for (i in 1:length(r_list)){
+  step <- r_list[[i]][[3]][[1]][[3]]
+  sub_list[[length(sub_list)+1]] <- step
 }
 
-try <- list()
+sub_list[c(183:202)]= NULL
 
-for(i in 1:100){
+# extract bills id
+
+id <- list()
+i <- 0
+j <- 0
+for(i in 1:length(sub_list)){
   for(j in 1:20){
-    step <- sponsor_list[[i]][[j]][[25]]
-    try[[length(try)+1]] <- step
+    step <- sub_list[[i]][[j]][[1]]
+    id[[length(id)+1]] <- step
   }
 }
 
-party <- as.data.frame(do.call(rbind, try))
-
-id <- (1:1650)
-party$id <-  id 
-
-
-
-pol_par<- as.data.frame(rep)
-pol_par$dem <- as.data.frame(dem)
-
-ggplot(pol_par, aes(x=pol_par)) +
-  geom_bar(stat = "identity") +
-  ggtitle("who sign more bill against climate changes") +
-  theme_bw()
-
-
-
-
-
-
-dat <- list()
-i <- 1
-j <- 1
-for(i in 1:100){
+# extract sponsor state
+s_s <- list()
+i <- 0
+j <- 0
+for(i in 1:length(sub_list)){
   for(j in 1:20){
-    step <- sponsor_list[[i]][[j]][[17]]
-    dat[[length(dat)+1]] <- step
+    step <- sub_list[[i]][[j]][[11]]
+    s_s[[length(s_s)+1]] <- step
   }
 }
 
-promulgate <- as.data.frame(do.call(rbind, dat))
-id <- (1:2000)
-promulgate$id <-  id 
+# extract sponsor party
+sp_p <- list()
+i <- 0
+j <- 0
+for(i in 1:length(sub_list)){
+  for(j in 1:20){
+    step <- sub_list[[i]][[j]][[12]]
+    sp_p[[length(sp_p)+1]] <- step
+  }
+}
+
+#extract introduced date
+
+i_date <- list()
+i <- 0
+j <- 0
+for(i in 1:length(sub_list)){
+  for(j in 1:20){
+    step <- sub_list[[i]][[j]][[17]]
+    i_date[[length(i_date)+1]] <- step
+  }
+}
+
+# extracty title
 
 tit <- list()
-i <- 1
-j <- 1
-for(i in 1:100){
+i <- 0
+j <- 0
+for(i in 1:length(sub_list)){
   for(j in 1:20){
-    step <- sponsor_list[[i]][[j]][[7]]
+    step <- sub_list[[i]][[j]][[6]]
     tit[[length(tit)+1]] <- step
   }
 }
 
-s_title <- as.data.frame(do.call(rbind, tit))
-id <- (1:2000)
-s_title$id <-  id 
 
 
-
-PS <- list()
-i <- 1
-j <- 1
-for(i in 1:100){
+# id senator who sponsored the bills 
+sen_id <- list()
+i <- 0
+j <- 0
+for(i in 1:length(sub_list)){
   for(j in 1:20){
-    step <- sponsor_list[[i]][[j]][[12]]
-    PS[[length(PS)+1]] <- step
+    step <- sub_list[[i]][[j]][[9]]
+    sen_id[[length(sen_id)+1]] <- step
+  }
+}
+
+# primary subject
+
+prim_subj <- list()
+i <- 0
+j <- 0
+for(i in 1:length(sub_list)){
+  for(j in 1:20){
+    step <- sub_list[[i]][[j]][[29]]
+    prim_subj[[length(prim_subj)+1]] <- step
   }
 }
 
 
 
-spo_par <- as.data.frame(do.call(rbind, PS))
-id <- (1:2000)
-spo_par$id <-  id 
+id <- as.data.frame(do.call(rbind, id))
+s_s <- as.data.frame(do.call(rbind, s_s))
+sp_p <- as.data.frame(do.call(rbind, sp_p))
+i_date  <- as.data.frame(do.call(rbind, i_date))
+tit <- as.data.frame(do.call(rbind, tit))
+sen_id <- as.data.frame(do.call(rbind, sen_id))
+prim_subj <- as.data.frame(do.call(rbind, prim_subj))
 
-party$R <- as.numeric(party$R)
-party$D <- as.numeric(party$D)
-rep <- sum(party$R)
-dem <- sum(party$D)
 
-write.csv(party, "party.csv")
-write.csv(promulgate, "promulgate.csv")
-write.csv(s_title, "stitle.csv")
-write.csv(spo_par, "sponsor_party.csv")
-write.csv(pol_par, "pp.csv")
+
+DF <- NULL
+DF <- data.frame(id, s_s, sp_p, i_date, sen_id, prim_subj, tit)
+
+colnames(DF) <- c("BILLS_id","sp_STATE","sp_PARTY","PROMULGATE","SEN_id","p_SUBJECT","TITLE")
+
+write.csv(DF, "data_from_api.csv")
+
+
+
+
+
